6811ca02da148f8f51fd8b8fbeda993140a11488
Soham Tagline <sht@taglineinfotech.com>
2024-03-26 10:58:54 +0530

    First Commit

---
+++ lib/esp_adapter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EspAdapter
+  VERSION = "0.1.0"
+end
---
+++ spec/esp_adapter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EspAdapter do
+  it "has a version number" do
+    expect(EspAdapter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
